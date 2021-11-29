68bcd6ed7e0e90b0864294e6e9f4ecfd79e68916
yam86rus <yam86rus@gmail.com>
2021-11-29 15:32:17 +0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainLayoutComponent} from "./shared/main-layout/main-layout.component";
+import {MainPageComponent} from "./shared/main-page/main-page.component";
+import {ProductPageComponent} from "./shared/product-page/product-page.component";
+import {CartPageComponent} from "./shared/cart-page/cart-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '',redirectTo:'/',pathMatch:'full'},
+      { path: '',component:MainPageComponent},
+      { path: 'product/:id', component:ProductPageComponent},
+      { path: 'cart',component: CartPageComponent}
+    ]
+  },
+  {
+    path: 'admin', loadChildren: ()=> import('./admin/admin.module').then(m=>m.adminModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
